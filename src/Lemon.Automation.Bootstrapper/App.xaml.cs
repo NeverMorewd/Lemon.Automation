52ec2f3d93d894af0ea77b866684f9b1713d59bc
easley <nevermore.wd@me.com>
2024-03-28 18:41:34 +0800

    IApplication &  App

---
+++ src/Lemon.Automation.Bootstrapper/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lemon.Automation.Bootstrapper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new();
+            window.Show();
+        }
+    }
+
+}
--- src/Lemon.Automation.Bootstrapper/Program.cs
+++ src/Lemon.Automation.Bootstrapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Lemon.Automation.Bootstrapper
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemon.Automation.Bootstrapper
 {
     internal class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TODO Whatever you want to do before starting
+            // the WPF application and loading all WPF dlls
+            RunApp();
+        }
+
+        // Ensure the method is not inlined, so you don't
+        // need to load any WPF dll in the Main method
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        static void RunApp()
+        {
+            //var app = new App();
+            //app.Run();
+
+            //var appStudio = new AppStudio();
+            //appStudio.Run();
+
+            var appUITracker = new AppUITracker();
+            appUITracker.Run();
         }
     }
 }

933c417ffe7caf55d6e172d5584fdcfb4e0ac36a
芬里尔吉崽 <nevermore.wd@me.com>
2024-04-22 16:52:00 +0800

    add win32

---
+++ src/Lemon.Native.Winx64/Natives/Window.cs
@@ -0,0 +1,13 @@
+﻿using Windows.Win32;
+
+namespace Lemon.Native.Winx64.Natives
+{
+    public static class Window
+    {
+        public static void Test()
+        {
+            //Invoke.En
+            //PInvoke
+        }
+    }
+}
